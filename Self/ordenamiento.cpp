1e4152bd0789ef44a2a8272b946709e454afe53f
L Æ L Ö <lalocornejoofficial@gmail.com>
2023-04-20 22:18:56 -0600

    ordenamieno

--- Self/ordenamiento.cpp
+++ Self/ordenamiento.cpp
@@ -2,23 +2,27 @@
 
 using namespace std;
 
-void ordenamiento(void);
-
 int main () {
-    ordenamiento();
-    return 0;
-}
-
-void ordenamiento() {
-    int num[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    int izquierda = 0 , mitad, derecha = 9;
+    int num[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int izquierda = 0 , mitad = 0, longitud = 10, derecha = (longitud - 1);
     int num_busqueda;
 
     cout << "Ingresa el numero a buscar: ";
     cin >> num_busqueda;
 
-    while(mitad != num_busqueda) {
-        mitad = izquierda + ((derecha - izquierda)/2);
+    while(num[mitad] != num_busqueda) {
 
+
+        mitad = izquierda + ((derecha - izquierda)/2);
+        
+        if(num[mitad] > num_busqueda){
+            derecha  = mitad;
+        }else if(num[mitad] < num_busqueda){
+            izquierda = mitad;
+        }
     }
+
+    cout << "El numero esta en la posicion: " << mitad;
+
+    return 0;
 }
