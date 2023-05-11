3f6bfd06e9830126e4b7389ce1747b119a51aeeb
L Æ L Ö <lalocornejoofficial@gmail.com>
2023-05-10 21:13:54 -0600

    gravity aceleration, still need the functions .-.

--- Self/Gravity.cpp
+++ Self/Gravity.cpp
@@ -1,23 +1,23 @@
 #include<iostream>
-#include<string>
+#include<string.h>
+#include<math.h>
+#include<random>
 #include<conio.h>
 #include<Windows.h>
 
-HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 void welcome();
 
 
-const int width = 150;
-const int height = 10;
-const int soil_lvl = height-3;
-const int gravity = 1;
-const int jump = 5;
-int acelev = 0;
-int aceleh = 0;
+const int width = 165;
+const int height = 14;
+const int soil_lvl = height-2;
+int gravity = 0;
+float acelerationV = 0, acelerationH = 0;
 
 bool simulator = false;
-int x, y;
+float x, y;
+int op;
 
 int tx[9400], ty[9400];
 
@@ -27,7 +27,7 @@ Direction dir;
 void Init() {
     simulator = true;
     dir = STOP;
-    x = 0;
+    x = 1;
     y = soil_lvl;
 
 }
@@ -35,13 +35,13 @@ void Init() {
 void Map(){
     system("cls");
     std::cout << "\n\n\n\n\n\n";
-    for(int i =0; i < height; i++){
-        for(int j=0; j < width; j++){
+    for(int i =0; i <= height; i++){
+        for(int j=0; j <= width; j++){
             if(j==0)
-                std::cout << "\t\t|";
+                std::cout << "\t  |";
             if(i == y && j == x)
                 std::cout << "x";
-            if(i == height-1)
+            if(i == height)
                 std::cout << "~";
             else{
                 bool print = false;
@@ -50,7 +50,7 @@ void Map(){
                     std::cout << " ";
                 }
             }
-            if(j == width - 1){
+            if(j == width){
                 std::cout << "|";
             }
         }
@@ -61,51 +61,57 @@ void Map(){
 void Input(){
     if(_kbhit()){
         switch(_getch()){
-            case 'a':
-                dir = LEFT;
-                break;
-            case 's':
-                dir = STOP;
-                break;
-            case 'd':
-                dir = RIGHT;
-                break;
-            case 'w':
-                dir = UP;
-                break;
+            case 'a': case 'A':
+            if(acelerationH > -4)
+                acelerationH -= 2;
+            break;
+            case 'd': case 'D':
+            if(acelerationH < 4)
+                acelerationH += 2;
+            break;
+            case 'w': case 'W':
+            if(y == 13)
+                acelerationV += rand() % 6;
+            break;
             default:
-                break;
+            break;
         }
-    }else
-        dir = STOP;
-
+    }
 } 
 
 void Gravity(){
-    y += gravity;
+    gravity = 1;
+        y += gravity;
+        if(y == 13)
+            gravity = abs(gravity);
+    if(acelerationV > 0){
+        acelerationV -= abs(gravity);
+    }
+}
+
+void Friction(){
+    if(y == 13 && acelerationH != 0 && acelerationH > 0){
+        acelerationH--;
+    }else if(y == 13 && acelerationH != 0 &&  acelerationH < 0){
+        acelerationH++;
+    }
 }
 
 void Logic(){
-    switch(dir){
-        case UP:
-            if(!(y<soil_lvl)){
-                y-=jump;
-            } 
-            break;
-        case LEFT:
-            x-=2;
-            break;
-        case RIGHT:
-            x+=2;
-            break;
-        default:
-            break;
+    if(acelerationH < 0){
+        x+=acelerationH; 
+    }
+    if(acelerationH > 0){
+        x+=acelerationH; 
+    }
+    if(acelerationV > 0){
+        y -= acelerationV;
     }
 
     if(y >= soil_lvl)
-        y= soil_lvl;
-    if(x >= width - 2)
-        x = width - 2;
+        y = soil_lvl;
+    if(x >= width - 1)
+        x = width - 1;
     if(x <= 0)
         x = 0;
 }
@@ -114,13 +120,34 @@ void Logic(){
 void StartSimul(){
     Init();
     while(simulator){
+        gravity = 0;
         Map();
         Input();
         Logic();
         Gravity();
+        Friction();
+        std::cout << "\t\t\t\t\t\t\t\t\t\t  Y: " << y << "  X: " << x;
+        std::cout << "\n\t\t\t\t\t\t\t\t\tAceleracion H: " << acelerationH << "  Aceleracion V: " << acelerationV;
+    }
+}
+
+void menu(){
+    std::cout << "**** Elige el modo de simula ****\n";
+    std::cout << "********* 1. Open world *********\n";
+    std::cout << "*********** 2. Battle ***********\n";
+    std::cout << "---------------------------------\n";
+    std::cout << "\tOpcion: "; std::cin >> op;
+
+    switch(op){
+        case 1:
+            StartSimul();
+            break;
+        
     }
 }
 
 int main(){
+    std::cin.tie(NULL);   
+
     StartSimul();
 }
