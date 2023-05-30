cbcc774b79e4b929bb88aaa6ea57713153b331d0
L Æ L Ö <lalocornejoofficial@gmail.com>
2023-05-29 20:54:42 -0600

    dsdfhadshgfkjasd

--- Prog Compet/Camp Jun/Ac-Jp.cpp
+++ Prog Compet/Camp Jun/Ac-Jp.cpp
@@ -1,4 +1,6 @@
 #include<bits/stdc++.h>
+#define ll long long int 
+#define vi vector<int>
 
 using namespace std;
 
@@ -12,21 +14,50 @@ int main(){
         ap = a;
 
         if(jp >= 1 && a < n-1){
-            if(ap + (k + 1) <= (n-1)){
+            if(ap + (k + 1) < (n)){
                 a = ap + (k+1);
-            }else if( ap + k <= (n-1)){
+                cout << "k+1" << k <<"\n";
+            }
+            else if( ap + k < (n)){
                 a = ap + (k);
-            }else if( ap + ( k - 1 ) <= (n-1)){
+                cout << "k" << k <<"\n";
+            }
+            else if( ap + ( k - 1 ) < (n)){
                 a = ap + (k-1);
+                cout << "k-1" << k <<"\n";
             }
                 jp++;
             
         }else if(jp < 1 || a == n-1){
+            cout << "asdas\n";
             a++;
             jp++;
 
         }
+        printf("a: %d jp: %d \n", a, jp);
     }
 
     printf("%d \n", jp);
 }
+
+/*int main(){
+    int previous=0, current=0, n, jp==0, inicial;
+
+    cin >> n;
+    inicial = n;
+
+    while(n > 0){
+        if(jp > 1 && n != 1){
+            current = inicial - n;
+            if()
+
+            swap(previous, current);
+
+        }else{
+            n--;
+            jp++;
+            current = 1;
+            swap(previous, current);
+        }
+    }
+}*/
