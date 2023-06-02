ecaed59fd460b1f8d7c657fc0b72dab74c45627d
L Æ L Ö <128832214+LaloCornejo@users.noreply.github.com>
2023-06-02 12:37:55 -0600

    Chocolates

---
+++ Prog Compet/Camp Jun/Chocolates.cpp
@@ -0,0 +1,22 @@
+//#include<bits/stdc++.h>
+#include<iostream>
+
+#define ll long long int
+using namespace std;
+
+int main() {
+    ll n;
+    cin >> int n;
+    ll chocolates[n], L, M, R;
+
+    for(int i = 0; i < n; i++){
+        cin >> chocolates[i];
+    }
+
+    for(int i = 0; i< n; i++){
+         = chocolates[i] - M;
+        chocolates[i] += 
+    }
+}
+
+//https://omegaup.com/arena/problem/Chocolates-CIIC-2015
