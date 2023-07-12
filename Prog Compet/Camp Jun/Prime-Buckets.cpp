62fe0c0bbd9d6c5c0ee55eedd6b70372c27b736c
L Æ L Ö <lalocornejoofficial@gmail.com>
2023-07-12 12:39:19 -0600

    primes ready  good O(n)

--- Prog Compet/Camp Jun/Prime-Buckets.cpp
+++ Prog Compet/Camp Jun/Prime-Buckets.cpp
@@ -2,21 +2,32 @@
 #define ll long long int 
 using namespace std;
 
+ll num;
+
+bool  isPrime(int num){
+    
+    if(num <= 1)
+         return false;
+
+    for(int i = 2; i <= sqrt(num); i++){
+        if(num % i == 0) 
+            return false;
+    }
+
+    return true;
+}
 int main() {
     ll n;
     cin >> n;
     bool primes[n];
     ll prim = 0;
 
-    for(int i = 0; i < n; i++) {
-        if((fact(n - 1) + 1) % n == 0) {
-            primes[i] = true;
-            cout << i << "\n";
-            prim++;
-        }else {
-            primes[i] = false;
+    for(num = 0; num <= n; num++)
+        if(isPrime(num)){
+            
+            primes[num] = true;
+            prim++;    
         }
-    }
 
-    cout << prim << "\n";
+    printf("Total Primes: %d \n", prim);
 }
