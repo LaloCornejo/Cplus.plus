79e40d6dd23604bf470d96dd018fe2b68033029c
Lalo Cornejo <lalocornejoofficial@gmail.com>
2023-09-12 23:07:59 -0600

    wawawa

--- School/2do/estructuradeDatos/wawawa.cpp
+++ School/2do/estructuradeDatos/wawawa.cpp
@@ -1,5 +1,23 @@
 #include<iostream>
 #include"lista.h"
+class lista{
+    private: 
+    int *a;
+    int n;
+    int MAX;
+
+public:
+    lista(int);
+    ~lista();
+    void mostrar_l();
+    void mostrar_e();
+    void buscar_e();
+    void anadir_e();
+    void insertar_e();
+    void eliminar_e();
+    void eliminar_todo_e();
+    void app();
+}
 
 using namespace std;
 
