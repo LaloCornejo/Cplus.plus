5e0702413d5357807f55fa305236a102c5421c6b
Lalo Conejo <lalocornejoofficial@gmail.com>
2023-11-08 10:45:02 -0600

    Encript correct :)

---
+++ Prog Compet/IDK/hfdhbkjdsfv.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#define ll long long int
+using namespace std;
+
+int main(){
+    ll t, n, x;
+
+    cin >> t;
+
+    for(int i = 0; i < t; i++){
+        cin >> n >> x;
+        ll ax;
+
+        int max = c[0];
+        for(int j = 0; i < n; j++){
+            cin >> ax;
+            
+        }
+    }
+
+}
--- School/2do/estructuradeDatos/Encript.cpp
+++ School/2do/estructuradeDatos/Encript.cpp
@@ -3,46 +3,54 @@
 #include<queue>
 using namespace std;
 
-bool isVowelOrSpace(char c) {
-    c = tolower(c);
-    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == ' ';
-}
-
-void encode(string mensaje) {
-    stack<char> s;
-    queue<char> q;
-
-    for (char c : mensaje) {
-        if (!isVowelOrSpace(c)) {
-            s.push(c);
-        } else {
-            while (!s.empty()) {
-                q.push(s.top());
-                s.pop();
-            }
-            q.push(c);
-        }
-    }
-
-    while (!s.empty()) {
-        q.push(s.top());
-        s.pop();
-    }
-
-    while (!q.empty()) {
-        cout << q.front();
-        q.pop();
-    }
-
-    cout << endl;
+bool isVowel( char m ){
+    if(m == 'a' || m == 'e' || m == 'i' || m == 'o' || m == 'u')
+        return true;
+    return false;
 }
 
 int main(){
-    string mensaje;
+    stack<char> s;
+    queue<char> q;
 
-    getline(cin, mensaje);
+    string message;
+    string F1, F2;
 
-    encode(mensaje);
+    getline(cin, message);
+    int l = message.length();
 
+    for(int i = 0; i < l; i++){
+        if(!isVowel(message[i])){
+            s.push(message[i]);
+        }else {
+            while(!s.empty()){
+                F1 += s.top();
+                s.pop();
+            }
+            F1 += message[i];
+        }
+    }
+
+    while(!s.empty()){
+        F1 += s.top();
+        s.pop();
+    }
+    
+    printf("Fase1: %s\n", F1.c_str());
+
+    for(int i = 0; i < l; i++){
+        s.push(F1[i]);
+        q.push(F1[i]);
+    }
+
+    for(int i = 0; i < l/2; i++){
+        F2 += q.front(); q.pop();
+        F2 += s.top(); s.pop();
+    }
+
+    if(l % 2 != 0)
+        F2 += q.front();
+
+    printf("%s\n", F2.c_str());
     return 0;
 }
